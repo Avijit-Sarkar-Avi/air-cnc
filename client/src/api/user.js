d96a483b53851b8266bd6af1a1eb5c48bc352662
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2023-05-02 18:09:28 +0600

    create an user api for post data in MongoDB

---
+++ client/src/api/user.js
@@ -0,0 +1,16 @@
+export const hostRequest = async hostData => {
+
+    const url = `${process.env.REACT_APP_API_URL}/user/${hostData?.email}`
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(hostData),
+    })
+
+    const data = await response.json()
+
+    return data
+}
